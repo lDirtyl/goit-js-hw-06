67bdbac8a75a806922a6e5e93c5301d6e641cc61
lDirtyl <andrii.mashtaler@gmx.at>
2024-07-09 15:49:55 +0200

    finish task-2

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+class Storage {
+  constructor(items) {
+    this._items = items;
+  }
+
+  getItems() {
+    return this._items;
+  }
+
+  addItem(newItem) {
+    return this._items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this._items = this._items.filter(item => item !== itemToRemove);
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
